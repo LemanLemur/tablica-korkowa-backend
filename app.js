622fa463b06c349bdd5c390f2e8466d888c07e63
Bartek <barteklemanski@o2.pl>
2020-03-24 21:08:51 +0100

    Views router

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var bodyParser = require("body-parser");
 
 //route ini
 var indexRouter = require('./routes/index');
+var viewsRouter = require('./routes/views');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //route use
 app.use('/', indexRouter);
+app.use('/views', viewsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
