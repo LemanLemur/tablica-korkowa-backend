bac77b64de92d7df08e12275865b718b6b8f58ca
Hinsiu <vaxes.hinc@gmail.com>
2020-03-27 22:10:19 +0100

    Cards API

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var viewsRouter = require('./routes/views');
 var subjectsRouter = require('./routes/subjects');
 var levelsRouter = require('./routes/levels');
+var cardsRouter = require('./routes/cards/');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -30,6 +31,7 @@ app.use('/', indexRouter);
 app.use('/views', viewsRouter);
 app.use('/subjects', subjectsRouter);
 app.use('/levels', levelsRouter);
+app.use('/cards/', cardsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
