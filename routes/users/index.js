91d0aa66b59ce426451c4268c67ffbcfb0c87b5d
Bartek <barteklemanski@o2.pl>
2020-04-07 21:25:18 +0200

    Users post remake

--- routes/users/index.js
+++ routes/users/index.js
@@ -2,15 +2,15 @@ var express = require("express");
 var router = express.Router();
 
 /* GET */
-router.get("/", function(req, res, next) {
+router.get("/", function (req, res, next) {
   const db = req.app.get("db");
   var output = [];
 
   db.collection("Users")
     .get()
-    .then(snapshot => {
+    .then((snapshot) => {
       if (snapshot) {
-        snapshot.forEach(doc => {
+        snapshot.forEach((doc) => {
           output.push({
             id: doc.id,
             accountId: doc.data().AccountID,
@@ -21,7 +21,7 @@ router.get("/", function(req, res, next) {
             lastName: doc.data().LastName,
             lastTimeLogged: doc.data().LastTimeLogged,
             telephone: doc.data().Telephone,
-            updated: doc.data().Updated
+            updated: doc.data().Updated,
           });
         });
         return res.status(200).json(output);
@@ -29,34 +29,34 @@ router.get("/", function(req, res, next) {
         return res.status(404).json({ message: "Any Users not found." });
       }
     })
-    .catch(error => {
+    .catch((error) => {
       return res
         .status(400)
         .json({ message: "Unable to connect to Firestore." });
     });
 });
 
-router.get("/:id", function(req, res, next) {
+router.get("/:id", function (req, res, next) {
   const db = req.app.get("db");
   var output = [];
 
   db.collection("Users")
     .doc(req.params.id)
     .get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-          output.push({
-            id: doc.id,
-            accountId: doc.data().AccountID,
-            avatar: doc.data().Avatar,
-            created: doc.data().Created,
-            email: doc.data().Email,
-            firstName: doc.data().FirstName,
-            lastName: doc.data().LastName,
-            lastTimeLogged: doc.data().LastTimeLogged,
-            telephone: doc.data().Telephone,
-            updated: doc.data().Updated
-          });
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        output.push({
+          id: doc.id,
+          accountId: doc.data().AccountID,
+          avatar: doc.data().Avatar,
+          created: doc.data().Created,
+          email: doc.data().Email,
+          firstName: doc.data().FirstName,
+          lastName: doc.data().LastName,
+          lastTimeLogged: doc.data().LastTimeLogged,
+          telephone: doc.data().Telephone,
+          updated: doc.data().Updated,
+        });
       });
       if (output.length === 0) {
         return res.status(404).json({ message: "Any Users not found." });
@@ -64,34 +64,34 @@ router.get("/:id", function(req, res, next) {
         return res.status(200).json(output);
       }
     })
-    .catch(error => {
+    .catch((error) => {
       return res
         .status(400)
         .json({ message: "Unable to connect to Firestore." });
     });
 });
 
-router.get("/accountid/:id", function(req, res, next) {
+router.get("/accountid/:id", function (req, res, next) {
   const db = req.app.get("db");
   var output = [];
   console.log(req.params.id);
   db.collection("Users")
-    .where('AccountID', '==', req.params.id)
+    .where("AccountID", "==", req.params.id)
     .get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-          output.push({
-            id: doc.id,
-            accountId: doc.data().AccountID,
-            avatar: doc.data().Avatar,
-            created: doc.data().Created,
-            email: doc.data().Email,
-            firstName: doc.data().FirstName,
-            lastName: doc.data().LastName,
-            lastTimeLogged: doc.data().LastTimeLogged,
-            telephone: doc.data().Telephone,
-            updated: doc.data().Updated
-          });
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        output.push({
+          id: doc.id,
+          accountId: doc.data().AccountID,
+          avatar: doc.data().Avatar,
+          created: doc.data().Created,
+          email: doc.data().Email,
+          firstName: doc.data().FirstName,
+          lastName: doc.data().LastName,
+          lastTimeLogged: doc.data().LastTimeLogged,
+          telephone: doc.data().Telephone,
+          updated: doc.data().Updated,
+        });
       });
       if (output.length === 0) {
         return res.status(404).json({ message: "Any Users not found." });
@@ -99,7 +99,7 @@ router.get("/accountid/:id", function(req, res, next) {
         return res.status(200).json(output);
       }
     })
-    .catch(error => {
+    .catch((error) => {
       return res
         .status(400)
         .json({ message: "Unable to connect to Firestore." });
@@ -114,22 +114,39 @@ router.post("/", (req, res) => {
   let lastname = req.body.lastname;
   let timestamp = new Date();
   let telephone = req.body.telephone;
+  let accountID = req.body.accountID;
+  let email = req.body.email;
+  let avatar = req.body.avatar;
+
+  if (telephone === "") {
+    telephone = "brak";
+  }
+  if (avatar === "") {
+    avatar =
+      "https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png";
+  }
 
   db.collection("Users")
     .add({
       FirstName: firstname,
       LastName: lastname,
       Created: timestamp,
-      Telephone: telephone
+      Telephone: telephone,
+      AccountID: accountID,
+      Email: email,
+      Avatar: avatar,
     })
-    .then(ref => {
+    .then((ref) => {
       if (ref.id) {
-        return res.status(200).json({ id: ref.id });
+        return res.status(200).json({
+          id: ref.id,
+        });
       } else {
         return res.status(400).json({ message: "Something went wrong." });
       }
     })
-    .catch(error => {
+    .catch((error) => {
+      console.log(error);
       return res
         .status(400)
         .json({ message: "Unable to connect to Firestore." });
@@ -155,12 +172,11 @@ router.put("/:id", (req, res) => {
 
   db.collection("Users")
     .doc(req.params.id)
-    .set(update, {merge: true}
-    )
+    .set(update, { merge: true })
     .then(() => {
       return res.status(200).json({ message: "Data updated." });
     })
-    .catch(error => {
+    .catch((error) => {
       return res
         .status(400)
         .json({ message: "Unable to connect to Firestore." });
@@ -175,23 +191,23 @@ router.put("/lastlogged/:id", (req, res) => {
     .doc(req.params.id)
     .set(
       {
-        LastTimeLogged: timestamp
+        LastTimeLogged: timestamp,
       },
       {
-        merge: true
+        merge: true,
       }
     )
     .then(() => {
       return res.status(200).json({ message: "Data updated." });
     })
-    .catch(error => {
+    .catch((error) => {
       return res
         .status(400)
         .json({ message: "Unable to connect to Firestore." });
     });
 });
 
-/* PUT */
+/* DELETE */
 
 router.delete("/:id", (req, res) => {
   const db = req.app.get("db");
@@ -201,7 +217,7 @@ router.delete("/:id", (req, res) => {
     .then(() => {
       return res.status(200).json({ message: "User deleted." });
     })
-    .catch(error => {
+    .catch((error) => {
       return res
         .status(400)
         .json({ message: "Unable to connect to Firestore." });
